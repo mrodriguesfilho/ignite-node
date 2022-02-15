c40bb6e71bf360c55843fa71caaaf01cecc90d29
Márcio <54534037+mrodriguesfilho@users.noreply.github.com>
2022-02-15 00:41:11 -0300

    Atualizando erros e o docker-compose

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -2,6 +2,7 @@ import { compare } from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
@@ -27,13 +28,13 @@ class AuthenticateUserUseCase {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
-      throw new Error("User or Password incorrect!");
+      throw new AppError("User or Password incorrect!");
     }
 
     const passwordMatch = await compare(password, user.password);
 
     if (!passwordMatch) {
-      throw new Error("User or Password incorrect!");
+      throw new AppError("User or Password incorrect!");
     }
 
     const token = sign({}, "ec31c480f1e8eca57a80509699be0c68", {
