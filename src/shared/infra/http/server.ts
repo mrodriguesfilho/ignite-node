b6dca7bb6c40880267118feb34e26460fd362cfa
Márcio <54534037+mrodriguesfilho@users.noreply.github.com>
2022-03-13 18:05:50 -0300

    Adicionei documentação no Swagger

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,11 +4,15 @@ import "../../container";
 import express, { Request, Response } from "express";
 
 import "express-async-errors";
+import swaggerUi from "swagger-ui-express";
+
 import { AppError } from "../../errors/AppError";
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 app.use((err: Error, request: Request, response: Response) => {
   if (err instanceof AppError) {
